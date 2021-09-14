2eb3c768b41fbb18574b2c5bd3ffe4ac2ec44332
Joe <joseph.allen.1994@hotmail.com>
2021-09-14 22:06:21 +0100

    Theming and variables refactor

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from './logo.svg';
 import './App.scss';
 import Example from './components/example';
+import Nav from './components/nav';
+import Footer from './components/footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>Navbar</p>
-      <p>content</p>
+        <div>
+        <Nav/>
       <Example></Example>
-      <p>footer</p>
+      <Footer/>
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const Nav=()=> {
+  return (
+    <>
+        <p>navbar</p>
+    </>
+  );
+}
+  
+export default Nav;
