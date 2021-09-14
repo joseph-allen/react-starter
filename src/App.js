a9e0c5111a964a4e1efadb7e82d8c99dcaff8ac3
Joe <joseph.allen.1994@hotmail.com>
2021-09-14 19:53:40 +0100

    Add SASS, bootstrap and theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Example from './components/example';
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Navbar</p>
+      <p>content</p>
+      <Example></Example>
+      <p>footer</p>
     </div>
   );
 }
